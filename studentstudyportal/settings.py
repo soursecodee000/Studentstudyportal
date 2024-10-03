33617dc31ea449f4104c1c068dfb7cda36253393
Muhammad Ahmad <ma7744746@gmail.com>
2024-10-03 07:32:35 +0500

    adding static settings

--- studentstudyportal/settings.py
+++ studentstudyportal/settings.py
@@ -118,15 +118,14 @@ USE_TZ = True
 import os
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-# URL for serving static files
+
 STATIC_URL = '/static/'
 
 # Directory where static files will be collected during deployment
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Directories where Django looks for static files during development
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-
+STATICFILES_DIRS = [BASE_DIR / 'static']
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
