36e9325880121e5d8a19f42e5cd859a732f0ea0d
Muhammad Ahmad <ma7744746@gmail.com>
2024-10-06 07:12:07 +0500

    deploying

--- studentstudyportal/settings/prod.py
+++ studentstudyportal/settings/prod.py
@@ -1,7 +1,13 @@
 from .common import *
 import os
+import dj_database_url
+
 DEBUG = False
 
 SECRET_KEY=os.environ['SECRET_KEY']
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['studentstudyportal-prod-848a5ccf8276.herokuapp.com']
+
+DATABASES = {
+    'default': dj_database_url.config()
+}
